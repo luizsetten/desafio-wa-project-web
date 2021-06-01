546edc2bd18399e0b50ba00dc55cc5cb59f67da5
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-01 08:12:34 -0300

    feat: add route and copy component

--- src/components/Pages/Admin/index.tsx
+++ src/components/Pages/Admin/index.tsx
@@ -1,12 +1,14 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Drawer from 'components/Layout/Drawer';
 import AccountMultipleIcon from 'mdi-react/AccountMultipleIcon';
+import ShoppingCartIcon from 'mdi-react/ShoppingCartIcon';
 import StarIcon from 'mdi-react/StarIcon';
 import ViewDashboardIcon from 'mdi-react/ViewDashboardIcon';
 import React, { memo, useCallback, useRef, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import DashboardIndexPage from './Dashboard';
+import OrderIndexPage from './Orders';
 import SamplePage from './Sample';
 import UserIndexPage from './Users';
 
@@ -43,6 +45,12 @@ const AdminPage = memo((props: {}) => {
       // role: enRoles.admin,
       icon: AccountMultipleIcon
     },
+    {
+      path: '/pedidos',
+      display: 'Pedidos',
+      // role: enRoles.admin,
+      icon: ShoppingCartIcon
+    },
     { path: '/exemplos', display: 'Exemplos', icon: StarIcon }
   ]);
 
@@ -57,6 +65,7 @@ const AdminPage = memo((props: {}) => {
             <Switch>
               <Route path='/exemplos' component={SamplePage} />
               <Route path='/usuarios' component={UserIndexPage} />
+              <Route path='/pedidos' component={OrderIndexPage} />
               <Route path='/' component={DashboardIndexPage} />
               <Route render={renderRedirect} />
             </Switch>
